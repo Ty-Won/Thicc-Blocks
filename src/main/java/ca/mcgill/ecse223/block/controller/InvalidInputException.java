8da6c27d958561ab520ef45691c0239e3a785022
msukkari <michael.sukkarieh@mail.mcgill.ca>
2019-02-19 19:26:18 -0500

    added new block223 and block223TO umple files

---
+++ src/main/java/ca/mcgill/ecse223/block/controller/InvalidInputException.java
@@ -0,0 +1,12 @@
+package ca.mcgill.ecse223.block.controller;
+
+public class InvalidInputException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8050736807295946879L;
+
+	public InvalidInputException(String message) {
+		super(message);
+	}
+}
