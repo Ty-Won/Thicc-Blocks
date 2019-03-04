276040fba2790924161d826507b803f15ee89582
msukkari <michael.sukkarieh@mail.mcgill.ca>
2019-03-04 10:39:19 -0700

    added test skeleton

---
+++ test/ca/mcgill/ecse223/block/application/Block223ApplicationTest.java
@@ -0,0 +1,16 @@
+package ca.mcgill.ecse223.block.application;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Block223ApplicationTest {
+
+	private static String filename = "testdata.btms";
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
