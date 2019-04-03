65b9a375852a8a8abf5cd7f0b826bd3455046616
Brendan Kellam <bshizzle1234@gmail.com>
2019-04-03 11:36:23 -0400

    Fixed testStartGameMovePaddleRightBeyondLimits test

--- src/main/java/ca/mcgill/ecse223/block/controller/Block223Controller.java
+++ src/main/java/ca/mcgill/ecse223/block/controller/Block223Controller.java
@@ -825,9 +825,9 @@ public class Block223Controller {
 		for (char c : userInputs.toCharArray()) {
 			int delta = 0;
 
-			if (c == 'l') {
+			if (c == 'l' && (game.getCurrentPaddleX() + PlayedGame.PADDLE_MOVE_LEFT) >= 0) {
 				delta = PlayedGame.PADDLE_MOVE_LEFT;
-			} else if (c == 'r') {
+			} else if (c == 'r' && (game.getCurrentPaddleX() + PlayedGame.PADDLE_MOVE_RIGHT) <= Game.PLAY_AREA_SIDE)  {
 				delta = PlayedGame.PADDLE_MOVE_RIGHT;
 			
 			// space
